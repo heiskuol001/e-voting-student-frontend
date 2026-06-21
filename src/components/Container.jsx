f9914be440b362a2bff66cf6b3ec20e3b21b5cc6
Kuol Magok <heiskuol001@gmail.com>
2026-06-21 21:05:14 +0300

    refactor: improve styling and layout of Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
 import { Bell, Clipboard } from 'lucide-react'
 
 const Container = () => {
     return (
-        <div className='hidden md:grid grid-cols-[2fr_1fr_1fr] gap-2 p-4'>
-            <section className=' gap-4 border rounded-md p-4'>
+        <div className='hidden md:grid grid-cols-[2fr_1fr_1fr] gap-2 p-3'>
+            <section className='gap-4 rounded-md border bg-white p-3'>
                 <div>
                     <h1 className='flex items-center gap-2'><Clipboard />Current Elections</h1>
                 </div>
                 <div></div>
             </section>
-            <section className='border rounded-md p-4'>
-                <h1 className='text-lg font-semibold text-gray-900'>Total Candidates</h1>
+            <section className='rounded-md border bg-white p-3'>
+                <h1 className='text-base font-semibold text-gray-900'>Total Candidates</h1>
             </section>
-            <section className='border rounded-md p-4'>
-                <div className='p-2'>
+            <section className='rounded-md border bg-white p-3'>
+                <div>
                     <h1 className='flex items-center gap-2'>
                         <Bell className='h-5 w-5' />
                         Notifications

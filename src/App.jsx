a85a518e67f1f26d2242ae906f625363e59f8071
Kuol Magok <heiskuol001@gmail.com>
2026-06-21 14:34:27 +0300

    add: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the E-Voting Student Frontend</h1>
+      <p>This is the main application component.</p>
+    </div>
+  )
+}
+
+export default App

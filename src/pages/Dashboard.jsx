48f22326da5c43cad95810c8e30540c32594a6d5
Kuol Magok <heiskuol001@gmail.com>
2026-06-22 12:12:37 +0300

    add: include Footer component in Dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,17 +5,18 @@ import UserInfo from '../components/UserInfo'
 import Election from '../components/Election'
 import Container from '../components/Container'
 import Container1 from '../components/Container1'
+import Footer from '../components/Footer'
 
 const Dashboard = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   return (
-    <div className='flex h-screen w-full overflow-hidden bg-gray-100'>
+    <div className='min-h-screen w-full bg-gray-100'>
       <Sidebar
         isOpen={isSidebarOpen}
         onClose={() => setIsSidebarOpen(false)}
       />
-      <main className='flex min-w-0 flex-1 flex-col overflow-y-auto overflow-x-hidden md:overflow-hidden'>
+      <main className='flex min-w-0 flex-1 flex-col overflow-x-hidden md:ml-20 lg:ml-64'>
         <Navbar
           isSidebarOpen={isSidebarOpen}
           onMenuClick={() => setIsSidebarOpen(true)}
@@ -24,6 +25,7 @@ const Dashboard = () => {
         <Election />
         <Container />
         <Container1 />
+        <Footer />
       </main>
     </div>
   )

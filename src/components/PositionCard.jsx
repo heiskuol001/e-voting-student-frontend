471a53dbc5e5d87f7c3e2f6af439108603d55a92
Kuol Magok <heiskuol001@gmail.com>
2026-06-22 12:13:20 +0300

    add: create PositionCard component for displaying position information

---
+++ src/components/PositionCard.jsx
@@ -0,0 +1,11 @@
+
+
+const PositionCard = () => {
+  return (
+    <div className='min-w-0 flex-1 rounded-md border px-3 py-2 sm:flex-none'>
+      <h1 className='truncate text-sm font-medium text-gray-900'>Guild President</h1>
+    </div>
+  )
+}
+
+export default PositionCard
